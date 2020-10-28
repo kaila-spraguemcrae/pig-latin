54bedc577baad4bddfbdc698a4e0ad7f5989fc86
Tawnee Harris <tawneeh@icloud.com>
2020-10-28 16:10:20 -0700

    add fourth passing test for pigLatin function
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
-function pigLatin(string) {
+function pigLatin(string){ 
   let fstLetter = string.charAt(0).toLowerCase();
   let fstSecLetter = (string.charAt(0).toLowerCase()).concat(string.charAt(1).toLowerCase())
+  let fstSecThiLetter = ((string.charAt(0).toLowerCase()).concat(string.charAt(1).toLowerCase()).concat(string.charAt(2).toLowerCase()))
   if (fstLetter=='a' || fstLetter=='e'|| fstLetter=='i' || fstLetter=='o' || fstLetter=='u') {
     return string + "way";
-  } else if (fstLetter!='a' && fstLetter!='e'&& fstLetter!='i' && fstLetter!='o' && fstLetter!='u' && fstSecLetter!='qu') {
+  } else if (fstLetter!='a' && fstLetter!='e'&& fstLetter!='i' && fstLetter!='o' && fstLetter!='u' && fstSecLetter!='qu' && fstSecThiLetter!='squ') {
     return string.slice(1) + string[0] + "ay";
   } else if (fstSecLetter == "qu") {
     return string.slice(2) + "quay";
-  } 
-
+  } else if (fstSecThiLetter == "squ") {
+    return string.slice(3) + "squay";
+  }
 }
