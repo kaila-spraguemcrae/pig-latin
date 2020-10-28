f8e99ca75c0bf7ed5def5734337413a61f2887f0
Tawnee Harris <tawneeh@icloud.com>
2020-10-28 14:46:09 -0700

    add passing test for function that will add 'way' to the end of words that begin with a vowel
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function pigLatin(string) {
+  let fstLetter = string.charAt(0).toLowerCase();
+  if (fstLetter=='a' || fstLetter=='e'|| fstLetter=='i' || fstLetter=='o' || fstLetter=='u') {
+    return string + "way";
+  }
+
+});
--- js/testscripts.js
+++ js/testscripts.js
@@ -1,25 +1,13 @@
-//Describe: `pigLatin()`
+//Describe: pigLatin()
 
-//Test: "It will add 'way' to the end of words that begin with a vowel"
+//Test: "It will add 'way' to the end of words that begin with 'a' 'e' 'i' 'o' or 'u'"
 //Expect(pigLatin("a")).toEqual("away");
 
-//Test: "It will move the first character of words that begin with consonants to the end of the word"
-//Expect:
+//Test: "It will move the first character of words that begin with consonants to the end of the word and add 'ay'"
+//Expect:(pigLatin("cat")).toEqual("atcay");
 
-//Test: "It will add 'ay' to the end of words that begin with a consonant"
-//Expect:
+//Test: "It will move the first two characters of words that begin with the characters 'q' and 'u' to the end of the word and add 'ay'"
+//Expect:(pigLatin("quit")).toEqual("itquay")
 
-//Test:
-//Expect:
-
-//Test:
-//Expect:
-
-//Test:
-//Expect:
-
-//Test:
-//Expect:
-
-//Test:
-//Expect:
+//Test: "It will move the first three characters of words that begin with the characters 's' 'q' and 'u' to the end of the word and add 'ay'"
+//Expect:(pigLatin("squid")).toEqual("idsquay")
