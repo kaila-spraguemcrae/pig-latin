f1d7b978979c382880a6e6826d8f42a69a630ff5
Tawnee Harris <tawneeh@icloud.com>
2020-10-28 15:38:52 -0700

    add third passing test for pigLatin function
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 function pigLatin(string) {
   let fstLetter = string.charAt(0).toLowerCase();
+  let fstSecLetter = (string.charAt(0).toLowerCase()).concat(string.charAt(1).toLowerCase())
   if (fstLetter=='a' || fstLetter=='e'|| fstLetter=='i' || fstLetter=='o' || fstLetter=='u') {
     return string + "way";
-  } else if (fstLetter!='a' && fstLetter!='e'&& fstLetter!='i' && fstLetter!='o' && fstLetter!='u') {
+  } else if (fstLetter!='a' && fstLetter!='e'&& fstLetter!='i' && fstLetter!='o' && fstLetter!='u' && fstSecLetter!='qu') {
     return string.slice(1) + string[0] + "ay";
-    } 
+  } else if (fstSecLetter == "qu") {
+    return string.slice(2) + "quay";
+  } 
 
-});
+}
