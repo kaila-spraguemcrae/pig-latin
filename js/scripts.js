a6d5fb386d42f737ff8fe4982937799a872e67de
Tawnee Harris <tawneeh@icloud.com>
2020-10-28 15:07:04 -0700

    add second passing test for pigLatin function
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ function pigLatin(string) {
   let fstLetter = string.charAt(0).toLowerCase();
   if (fstLetter=='a' || fstLetter=='e'|| fstLetter=='i' || fstLetter=='o' || fstLetter=='u') {
     return string + "way";
-  }
+  } else if (fstLetter!='a' && fstLetter!='e'&& fstLetter!='i' && fstLetter!='o' && fstLetter!='u') {
+    return string.slice(1) + string[0] + "ay";
+    } 
 
 });
